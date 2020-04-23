bf229572df89d6c7af9bcf9e8534815c0560889a
user <user@DESKTOP-LDANAR7>
2020-04-23 14:21:21 +0900

    dkanrjsk

---
+++ project/src/main/java/com/krahs123/project/model/UserVO.java
@@ -0,0 +1,68 @@
+package com.krahs123.project.model;
+
+public class UserVO {
+	private int id;
+	private String userid;
+	private String passwd;
+	private String name;
+	private String phone;
+	private String email;
+	private String domain;
+	private String auth;
+	private String regdate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getAuth() {
+		return auth;
+	}
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+}
