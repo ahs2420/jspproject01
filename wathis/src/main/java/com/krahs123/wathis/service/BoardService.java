869b2df418decdcd24aaafd39695383ff4a66246
user <user@CW-22>
2020-04-27 14:50:39 +0900

    service 파일 재생성

---
+++ wathis/src/main/java/com/krahs123/wathis/service/BoardService.java
@@ -0,0 +1,5 @@
+package com.krahs123.wathis.service;
+
+public interface BoardService {
+
+}
---
+++ wathis/src/main/java/com/krahs123/wathis/service/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.krahs123.wathis.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+}
